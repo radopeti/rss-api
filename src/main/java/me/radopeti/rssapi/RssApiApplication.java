b8feeb5e4d48f4bbf25ed5193ef0c8d5069929a8
Peter Rado <rado.peti.subs@gmail.com>
2023-09-24 12:46:05 +0200

    init project

---
+++ src/main/java/me/radopeti/rssapi/RssApiApplication.java
@@ -0,0 +1,13 @@
+package me.radopeti.rssapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/radopeti/rssapi/RssApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.radopeti.rssapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
