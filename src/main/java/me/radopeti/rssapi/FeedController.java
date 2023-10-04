65acc7637f2836c336f78563da1c366575ad8894
Peter Rado <rado.peti.subs@gmail.com>
2023-10-04 22:04:21 +0200

    change deprecated XmlReader to InputStream as feed input

--- src/main/java/me/radopeti/rssapi/FeedController.java
+++ src/main/java/me/radopeti/rssapi/FeedController.java
@@ -13,7 +13,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
+
+import static java.net.http.HttpRequest.newBuilder;
+import static java.net.http.HttpResponse.BodyHandlers;
 
 @RestController
 @RequestMapping("/feed")
@@ -21,9 +28,16 @@ public class FeedController {
 
     @CrossOrigin
     @GetMapping()
-    public ResponseEntity<FeedResponse> getFeed(@RequestParam("url") String url) throws IOException, FeedException {
+    public ResponseEntity<FeedResponse> getFeed(@RequestParam("url") String url) throws IOException, FeedException, URISyntaxException, InterruptedException {
         //String url = "https://danaepp.com/feed";
-        SyndFeed feed = new SyndFeedInput().build(new XmlReader(new URL(url)));
+        final HttpResponse<InputStream> response =
+                HttpClient.newHttpClient()
+                        .send(
+                                newBuilder().GET().uri(new URL(url).toURI()).build(),
+                                BodyHandlers.ofInputStream()
+                        );
+        SyndFeed feed = new SyndFeedInput().build(new XmlReader(response.body()));
+
         final FeedResponse feedResponse = new FeedResponse(Jsoup.parse(feed.getEntries().get(0).getContents().get(0).getValue()).text());
 
         return ResponseEntity.ok(feedResponse);
